04e4486bbb755e51e6809b8ddd7a1950b9b1bfa0
bewisesh91 <bewise.seunghyun@gmail.com>
2021-07-07 13:56:15 +0900

    create spring files

--- Desktop/java_spring/week01/src/main/java/com/spring/week01/prac/Prac.java
+++ Desktop/java_spring/week01/src/main/java/com/spring/week01/prac/Prac.java
@@ -16,6 +16,31 @@ public class Prac {
         System.out.println("별점: " + ratings);
     }
 
+    public static int countFruit(String fruit) {
+        List<String> fruits = new ArrayList<>();
+        fruits.add("감");
+        fruits.add("배");
+        fruits.add("감");
+        fruits.add("딸기");
+        fruits.add("수박");
+        fruits.add("메론");
+        fruits.add("수박");
+        fruits.add("딸기");
+        fruits.add("메론");
+        fruits.add("수박");
+        fruits.add("메론");
+        fruits.add("수박");
+        fruits.add("감");
+
+        int cnt = 0;
+        for (String i :fruits) {
+            if (i == fruit) {
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+
 
     public static void main(String[] args) {
         System.out.println("Hello, World");
@@ -34,5 +59,41 @@ public class Prac {
         System.out.println(myList);
 
         printInfo();
+
+        List<String> fruits = new ArrayList<>();
+        fruits.add("감");
+        fruits.add("배");
+        fruits.add("감");
+        fruits.add("딸기");
+        fruits.add("수박");
+        fruits.add("메론");
+        fruits.add("수박");
+        fruits.add("딸기");
+        fruits.add("메론");
+        fruits.add("수박");
+        fruits.add("메론");
+        fruits.add("수박");
+        fruits.add("감");
+        System.out.println(fruits);
+
+        for (String fruit : fruits) {
+            System.out.println(fruit);
+        }
+
+        int age = 20;
+        if (age > 19) {
+            System.out.println("성인입니다.");
+        } else {
+            System.out.println("미성년자입니다.");
+        }
+
+        int melon = countFruit("메론");
+        System.out.println(melon);
+
+        Course course = new Course();
+        course.setTitle("웹개발의 봄, spring");
+        System.out.println(course.getTitle());
+        System.out.println(course.getTutor());
+
     }
 }
